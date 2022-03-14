4fd610afb9b90ca48925f17139841ed3e29338db
Vagner Urata <vagnerurata@gmail.com>
2022-03-14 19:17:26 -0300

    Metodo ModelMapper implementado no DTO e Entity

--- src/main/java/com/vagnerurata/domain/Patient.java
+++ src/main/java/com/vagnerurata/domain/Patient.java
@@ -9,6 +9,10 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import org.modelmapper.ModelMapper;
+
+import com.vagnerurata.domain.dto.PatientDTO;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,23 +21,25 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name ="tb_patient")
+@Table(name = "tb_patient")
 public class Patient implements Serializable {
-	
+
 	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
-	
-	private String name;
-	
-	private Date birthDate;
-	
-	private String email;
-	
-	private String address;
-	
-	
 
+	private String name;
+
+	private Date birthDate;
+
+	private String email;
+
+	private String address;
+
+	public PatientDTO toDto() {
+		var mapper = new ModelMapper();
+		return mapper.map(this, PatientDTO.class);
+	}
 }
--- src/main/java/com/vagnerurata/domain/dto/PatientDTO.java
+++ src/main/java/com/vagnerurata/domain/dto/PatientDTO.java
@@ -3,6 +3,10 @@ package com.vagnerurata.domain.dto;
 import java.io.Serializable;
 import java.util.Date;
 
+import org.modelmapper.ModelMapper;
+
+import com.vagnerurata.domain.Patient;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,18 +15,21 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class PatientDTO implements Serializable {
-	
+
 	private static final long serialVersionUID = 1L;
 
 	private Integer id;
-	
+
 	private String name;
-	
+
 	private Date birthDate;
-	
+
 	private String email;
-		
+
 	private String address;
 
-	
+	public Patient toEntity() {
+		var mapper = new ModelMapper();
+		return mapper.map(this, Patient.class);
+	}
 }

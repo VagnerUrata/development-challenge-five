b7a67b4492509f127d33730437032b83ff2ec807
Vagner Urata <vagnerurata@gmail.com>
2022-03-14 19:00:07 -0300

    Criado domain patient

---
+++ src/main/java/com/vagnerurata/domain/Patient.java
@@ -0,0 +1,39 @@
+package com.vagnerurata.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name ="tb_patient")
+public class Patient implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	
+	private String name;
+	
+	private Date birthDate;
+	
+	private String email;
+	
+	private String address;
+	
+	
+
+}
